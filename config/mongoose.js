96a43087bec4a2107132d92e287ac1ec6b97c8d7
Rahul kumar <rk152531@gmail.com>
2023-08-10 14:13:05 +0530

    modified

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,19 +1,33 @@
+// const mongoose = require("mongoose");
+// const env = require('./environment');
+// mongoose.set("strictQuery", true);
+// mongoose.connect('mongodb://127.0.0.1:27017/codeial123');
+
+// const db = mongoose.connection;
+
+// db.on(
+//     "error",
+//     console.error.bind(console, "Error connection to database MangoDB")
+// );
+
+// db.once("open", function () {
+//     console.log("Connected to Database :: MongoDB");
+// });
+
+// module.exports = db;
+// // export default db;
+// // export {db};
+
 const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', true);
-
-const env = require('./environment')
-
-mongoose.connect(`mongodb://127.0.0.1/${env.db}`);
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-
-db.once('open', function () {
-    console.log('Connected to Database :: MongoDB');
-});
-
-
-module.exports = db;
+async function ConnectingToDB() {
+    try {
+        await mongoose.connect('mongodb+srv://rk152531:8591@codeial.fhi4bjp.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+        console.log('Connected to DB');
+      } catch (error) {
+        console.log("Error connecting to DB:" ,error);
+      }
+}
+ConnectingToDB();
--- workers/comment_email_worker.js
+++ workers/comment_email_worker.js
@@ -2,7 +2,7 @@ const queue = require('../config/kue');
 
 const commentsMiler = require('../mailers/comments_mailer');
 
-queue.process('emails', function(job, done){
+queue.process('emails', function (job, done) {
     console.log('emails worker is processing a job', job.data);
     commentsMiler.newComment(job.data);
     done();
